7957a8bd7c974a6e393c9a6c4d310d8b4bf9dec1
ChadiElHanchi <chadielhanchi@gmail.com>
2023-12-20 09:02:21 -0800

    l

--- factors.py
+++ factors.py
@@ -1,32 +1,67 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
+import time
+import math
 import sys
 
-def factorize(number):
-    factors = []
-    for i in range(2, int(number**0.5) + 1):
-        while number % i == 0:
-            factors.append(i)
-            number //= i
-    if number > 1:
-        factors.append(number)
-    return factors
-
-def main(filename):
-    with open(filename, 'r') as file:
-        content = file.read()
-
-    numbers = [int(num) for num in content.replace('\\n', '\n').split('\n') if num]
+def pollard_rho(n):
+    if n % 2 == 0:
+        return 2
     
-    for num in numbers:
-        factors = factorize(num)
-        print(f"{num}={'*'.join(map(str, factors))}")
+    x = 2
+    y = 2
+    d = 1 
+    # Define the function f(x) using a lambda function
+    f = lambda x: (x**2 + 1) % n
+    # Loop until a non-trivial factor is found
+    while d == 1:
+        # Generate new values for x using the function f(x)
+        x = f(x)
+	
+        # Generate new values for y using the function f(f(y))
+        y = f(f(y))
+        # Calculate the greatest common divisor between |x - y| and n
+        d = math.gcd(abs(x - y), n)
+    # Return the non-trivial factor
+    return d
 
-if __name__ == "__main__":
+def main():
+    # Check if the correct number of command-line arguments is provided
     if len(sys.argv) != 2:
-        print("Usage: python factors.py <file>")
-        sys.exit(1)
+        # Print the correct usage of the program
+        print("Usage: python factorize.py <file>")
+        # Exit the program if the correct arguments are not provided
+        return
+    # Get the file path from the command-line argument
+    file_path = sys.argv[1]
+    # start timing
+    start_time = time.time()
+    try:
+        # Open the file in read mode
+        with open(file_path, 'r') as file:
+	# Read all lines of the file into a lis
+            numbers = file.readlines()
+        # Iterate through each number in the list
+        for number in numbers:
+	# Convert the string number to an integer
+            num = int(number.strip())
+	# Factorize the number using Pollard's rho algorithm
+            factor = pollard_rho(num)
+            # If the factor is equal to the original number
+            if factor == num:
+                # Print that the number is prime
+                print(f"{num} is prime.")
+            else:
+	    # Print the factors of the number
+                print(f"{num}={factor}*{num // factor}")
 
-    filename = sys.argv[1]
-    main(filename)
+	if time.time() - start_time > 5:
+                print("Time limit exceeded")
+                exit()
+
+    # Print an error message if the file is not found
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+
+if __name__ == '__main__':
+    main()  # Call the main function when the script is executed
 
